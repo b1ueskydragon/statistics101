b208015fefbca02edd4aea6a424c5a0d7aba399d
ina <dragoalie@gmail.com>
2024-11-20 12:51:32 +0900

    Enhance test cases and comments

--- src/main/java/statistics101/util/KahanSum.java
+++ src/main/java/statistics101/util/KahanSum.java
@@ -1,6 +1,12 @@
 package statistics101.util;
 
+import java.util.Arrays;
+
 /**
+ * Kahan Sum のゴール:
+ * - 足し算における丸め誤差の補正
+ * - 足し算で生じる丸め誤差に特化したアルゴリズム
+ * <p>
  * java の double 型は 64ビットの浮動小数点
  * IEEE754仕様で 52 + 1 ビットが実際の有効数字であり
  * 10進数表現の有効数字だと
@@ -8,44 +14,66 @@ package statistics101.util;
  * <p>
  * ちなみに
  * - 丸め誤差(Rounding Error): 計算途中「小さすぎる」値が失われる. 一応計算は続行できる
- * - 桁溢れ(Overflow): 計算途中桁が足りず、計算続行できない
+ * - 桁溢れ(Overflow): 計算途中桁が足りず, 計算続行できない
  * - Kahan-sum も Overflow, Underflow には対応しない
  */
 public class KahanSum {
+
+    private static final String FORMAT = "%.16f%n";
+
     static double kahanSum(double[] x) {
         // ゴールとする合計
         double sum = 0;
         // 補正値: A running compensation for lost low-order bits
         double c = 0;
         for (double xi : x) {
-            // 補正後の値
+            // y は補正後の値
             // c は前回の補正
+            //
+            // なぜ引き算をするのか?
+            // - 補正自体が丸められることを防げる
+            // - 符号を反転させた引き算によって, 丸められた分を正しく補える
+            // - 引き算を使うことで, 丸め誤差を「逆向き」に記録
+            // - 次の計算で, その逆向きの値を引くことで (実質的に足すことで) 補正
+            //
+            // ちなみに足し算で保証すると...
+            // - 補正値 c を足すとまた丸め誤差が発生し, 補正の意味が失われる
+            // - 特に小さい値（c）を大きな値（xi）に足すと、小さい値が再び無視されてしまう
             double y = xi - c;
 
             // 仮の和. 丸め誤差が発生する恐れがある
             // sum + y が桁から溢れる場合. y は失われる（丸められる）
             // これまでの sum = 1.0,  y = 1e-16 の例だと
-            // 1e-16 の表現には問題ない(重要)、1.0 + 1e-16の演算には有効数字で表現できず
+            // 1e-16 の表現には問題ない(重要), 1.0 + 1e-16の演算には有効数字で表現できず
             // 結果が丸められる
             // 計算後は 1e-16 が失われ t = 1.0 となる
             double t = sum + y;
 
             // 補正値の更新
+            // 失われた値の「逆」(符号反転値) を補正値として保持
+            // 次の loop で失われた分を引き算することで足し戻す
+            // これまでの sum = 1.0,  y = 1e-16 の例だと, 失われた 1e-16が -1e-16 として保持され
+            // 次の loop で -(-1e-16) することで　+1e-16 として補うことが実現できる
             c = (t - sum) - y;
 
             // 合計値の更新
             sum = t;
         }
-        System.out.printf("Kahan-sum: %.16f%n", sum);
+        System.out.printf("Kahan-sum: " + FORMAT, sum);
         return sum;
     }
 
-    static double plainForLoopSum(double[] x) {
+    // stream#sum は比較的正確
+    static double streamSum(double[] x) {
+        double sum = Arrays.stream(x).sum();
+        System.out.printf("stream-sum: " + FORMAT, sum);
+        return sum;
+    }
+
+    static double sumWithoutCompensation(double[] x) {
         double sum = 0;
-        for (double xi : x) {
-            sum += xi;
-        }
-        System.out.printf("plain-for-loop-sum: %.16f%n", sum);
+        for (double xi : x) sum += xi;
+        System.out.printf("plain-for-loop-sum: " + FORMAT, sum);
         return sum;
     }
 }
--- src/test/java/statistics101/util/KahanSumTest.java
+++ src/test/java/statistics101/util/KahanSumTest.java
@@ -1,40 +1,68 @@
 package statistics101.util;
 
-import java.util.Arrays;
+import java.math.BigDecimal;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static statistics101.util.KahanSum.kahanSum;
-import static statistics101.util.KahanSum.plainForLoopSum;
+import static statistics101.util.KahanSum.streamSum;
+import static statistics101.util.KahanSum.sumWithoutCompensation;
 
+@SuppressWarnings("PrimitiveArrayArgumentToVariableArgMethod")
 class KahanSumTest {
     @ParameterizedTest
     @MethodSource
     void sum(double[] x) {
-        // stream#sum は比較的正確
-        double actual = kahanSum(x);
-        assertEquals(Arrays.stream(x).sum(), actual);
-        assertNotEquals(plainForLoopSum(x), actual);
+        double streamSum = streamSum(x);
+        double kahanSum = kahanSum(x);
+        assertEquals(streamSum, kahanSum);
+        assertNotEquals(sumWithoutCompensation(x), kahanSum);
     }
 
     static Stream<Arguments> sum() {
         // Plain for-loop sum において浮動小数点演算による誤差があり得る例を集める
         return Stream.of(
+                // 1e-16 == 10^(-16) == 0.0000000000000001
                 // 例えば 1 + 1e-16 の 1e-16 が丸め誤差で消える
-                // 1e-16 の表現には問題ないが、1.0 と 1e-16の演算には有効数字が足りず、結果が丸められる
+                // 1e-16 の表現には問題ないが, 1.0 と 1e-16の演算には有効数字が足りず, 結果が丸められる
                 arguments(new double[]{1.0, 1e-16, -1.0}),
                 // standardization で扱った zスコア変換後の配列
                 arguments(new double[]{-1.521, -1.217, -0.913, -0.609, 0.0, 0.0, 0.609, 0.913, 1.217, 1.521})
         );
     }
 
-    //  Kahan-sum も Overflow, Underflow には対応しない
-//    void overFlowCase() {
-//        final double[] x = {1e308, 1e308, -1e308};
-//    }
+    // Kahan-sum, Arrays#stream#sum どちらも丸められるケース
+    @Test
+    void roundingErrorCase() {
+        // precise sum should be 2.0000000000000001
+        // but 1e-16 will be lost
+        //
+        // [1.0, 1e-16, -1.0] : 大きな値が相殺され, 補正することで小さな値を残せる
+        // [1.0, 1e-16, 1.0] : 大きな値は相殺されない(大きな値が支配的), 小さな値は失われる
+        // そもそも 2.0000000000000001 が double で正確に表現できない
+        final double[] x = new double[]{1.0, 1e-16, 1.0};
+        assertEquals(2.0000000000000000, streamSum(x));
+        assertEquals(2.0000000000000000, kahanSum(x));
+
+        // BigDecimal を使わない限り丸められる
+        final var a = new BigDecimal("1.0");
+        final var b = new BigDecimal("1e-16");
+        final var c = new BigDecimal("1.0");
+        assertEquals(new BigDecimal("2.0000000000000001"), a.add(b).add(c));
+    }
+
+    // Kahan-sum, Arrays#stream#sum どちらもOverflow や Underflow には対応しない
+    @Test
+    void overFlowCase() {
+        final double[] x = {1e308, 1e308, -1e308};
+        assertTrue(Double.isInfinite(streamSum(x)));
+        assertTrue(Double.isNaN(kahanSum(x)));
+    }
 }
